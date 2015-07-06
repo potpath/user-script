77315edbd77894aff19f90c53c64639481467f93
Mana <mana_ohm@hotmail.com>
2015-07-06 10:01:48 +0700

    Add netauth Chula

---
+++ netauth_Chula.user.js
@@ -0,0 +1,16 @@
+﻿// ==UserScript==
+// @name        netauth Chula
+// @description check accept agreement, auto focus captcha and submit
+// @include     https://netauth.it.chula.ac.th/user/Logon.do
+// @version     0.2
+// @grant       none
+// @namespace https://greasyfork.org/users/4947
+// ==/UserScript==
+
+checkY.checked=true;
+var verificationCode=document.getElementsByName('verificationCode')[0];
+verificationCode.addEventListener('input',function(){
+	verificationCode.value.length==5 && document.forms[0].submit();
+},true);
+verificationCode.focus();
+verificationCode.autocomplete='off';
